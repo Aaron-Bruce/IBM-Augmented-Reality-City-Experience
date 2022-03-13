8c83afbaba2310a224b1e6dce9cf7285d761c2e3
Conn Breathnach <cobreath@tcd.ie>
2022-03-13 21:10:09 +0000

    Refactored project code

---
+++ public/javascript/places.js
@@ -0,0 +1,127 @@
+window.onload = () => {
+    let method = 'dynamic';
+
+    // if you want to statically add places, de-comment following line
+    // method = 'static';
+
+    if (method === 'static') {
+        let places = staticLoadPlaces();
+        renderPlaces(places);
+    }
+
+    else {
+
+        // first get current user location
+        return navigator.geolocation.getCurrentPosition(function (position) {
+
+                // than use it to load from remote APIs some places nearby
+                dynamicLoadPlaces(position.coords)
+                    .then((places) => {
+                        renderPlaces(places);
+                    })
+            },
+            (err) => console.error('Error in retrieving position', err),
+            {
+                enableHighAccuracy: true,
+                maximumAge: 0,
+                timeout: 27000,
+            }
+        );
+    }
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: "Your place name",
+            location: {
+                lat: 0, // add here latitude if using static data
+                lng: 0, // add here longitude if using static data
+            }
+        },
+        {
+            name: 'Another place name',
+            location: {
+                lat: 0,
+                lng: 0,
+            }
+        }
+    ];
+}
+
+// getting places from REST APIs
+function dynamicLoadPlaces(position) {
+    let params = {
+        radius: 300,    // search places not farther than this value (in meters)
+        clientId: '4XPNNSPPBLJSFTNKWKOLPD2LNG1U1QPOQ3GTGD1BNQB5YMUB', // add your credentials here
+        clientSecret: 'WFXGGXSNM0URBR3BS4GTGQG0U2OJRA43VPOS5EYEXGYWC5SR',   // add your credentials here
+        version: '20220311',    // foursquare versioning, required but unuseful for this demo
+    };
+    const options = {method: 'GET', headers: {Accept: 'application/json', Authorization: 'fsq3CZV50/mx2clbYgtx+2tmhtOglLrczuS+QLniakahIKs='}};
+    // CORS Proxy to avoid CORS problems
+    let corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
+    // Foursquare API
+    let endpoint = `https://api.foursquare.com/v3/places/nearby
+        &ll=${position.latitude},${position.longitude}
+        &hacc=${params.radius}
+        &limit=15
+        &v=${params.version}`;
+    return fetch(endpoint, options)
+        .then((res) => {
+            console.log(res)
+            return res.json()
+                .then((resp) => {
+                    return resp.response.venues;
+                })
+        })
+        .catch((err) => {
+            console.error('Error with places API', err);
+        })
+};
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        const latitude = place.location.lat;
+        const longitude = place.location.lng;
+
+        // add place icon
+        const icon = document.createElement('a-image');
+        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+        icon.setAttribute('name', place.name);
+        icon.setAttribute('src', '../assets/map-marker.png');
+
+        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+        icon.setAttribute('scale', '20, 20');
+
+        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+
+        const clickListener = function (ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
+
+            const name = ev.target.getAttribute('name');
+
+            const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+            if (el && el === ev.target) {
+                const label = document.createElement('span');
+                const container = document.createElement('div');
+                container.setAttribute('id', 'place-label');
+                label.innerText = name;
+                container.appendChild(label);
+                document.body.appendChild(container);
+
+                setTimeout(() => {
+                    container.parentElement.removeChild(container);
+                }, 1500);
+            }
+        };
+
+        icon.addEventListener('click', clickListener);
+
+        scene.appendChild(icon);
+    });
+}
