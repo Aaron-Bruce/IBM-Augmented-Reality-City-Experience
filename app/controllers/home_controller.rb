d2a805a2f46f6ebd09ac0cace18424341f9aee21
Aranq13 <98594133+Aranq13@users.noreply.github.com>
2022-03-24 12:47:09 +0000

    Parse data received from API into a ruby array

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -44,6 +44,7 @@
 class HomeController < ApplicationController
   def home
   require "uri"
+  require "json"
   require "net/http"
 
   url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=53.3444407%2C-6.2595753&radius=750&type=restaurant&key=YOUR_KEY_HERE")
@@ -55,5 +56,6 @@ class HomeController < ApplicationController
 
   response = https.request(request)
   puts response.read_body
+  @data = JSON.parse(response)
   end
 end
