f380ff1c3027a6b5a20667d14614c0c21c33569f
Conn Breathnach <38386940+ConnBreathnach@users.noreply.github.com>
2022-03-26 23:46:17 +0000

    Update home_controller.rb
    Removed unnecessary comments and changed JSON parst for response.read_body rather than response.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,46 +1,3 @@
-#Retrieving a list of spots
-#Retrieve spot with latittude and longitude:
-#@client.spots(-33.8670522, 151.1957362)
-#Retrieve spot with specific type:
-#@client.spots(-33.8670522, 151.1957362, :types => 'restaurant')
-#Retrieve spot with multiple types:
-#@client.spots(-33.8670522, 151.1957362, :types => ['restaurant','food'])
-#Search by multiple types but exclude multiple types:
-#@client.spots(-33.8670522, 151.1957362, :types => ['restaurant','food'], :exclude => ['cafe', 'establishment'])
-#Search by name:
-#@client.spots(-33.8670522, 151.1957362, :name => 'italian')
-#Search by name and type:
-#@client.spots(-33.8670522, 151.1957362, :name => 'italian', :types => 'restaurant')
-#Search in a radius (in meters):
-#@client.spots(-33.8670522, 151.1957362, :radius => 100)
-#Get results in specific language:
-#@client.spots(-33.8670522, 151.1957362, :language => 'en')
-#Retrieving spots based on query
-#@client.spots_by_query('Pizza near Miami Florida')
-#Search by multiple types and exclude multiple types
-#@client.spots_by_query('Pizza near Miami Florida', :types => ['restaurant', 'food'], :exclude => ['cafe', 'establishment'])
-#Retrieving a list of spots
-#Retrieve spot with latittude and longitude:
-#@client.spots(-33.8670522, 151.1957362)
-#Retrieve spot with specific type:
-#@client.spots(-33.8670522, 151.1957362, :types => 'restaurant')
-#Retrieve spot with multiple types:
-#@client.spots(-33.8670522, 151.1957362, :types => ['restaurant','food'])
-#Search by multiple types but exclude multiple types:
-#@client.spots(-33.8670522, 151.1957362, :types => ['restaurant','food'], :exclude => ['cafe', 'establishment'])
-#Search by name:
-#@client.spots(-33.8670522, 151.1957362, :name => 'italian')
-#Search by name and type:
-#@client.spots(-33.8670522, 151.1957362, :name => 'italian', :types => 'restaurant')
-#Search in a radius (in meters):
-#@client.spots(-33.8670522, 151.1957362, :radius => 100)
-#Get results in specific language:
-#@client.spots(-33.8670522, 151.1957362, :language => 'en')
-#Retrieving spots based on query
-#@client.spots_by_query('Pizza near Miami Florida')
-#Search by multiple types and exclude multiple types
-#@client.spots_by_query('Pizza near Miami Florida', :types => ['restaurant', 'food'], :exclude => ['cafe', 'establishment'])
-
 class HomeController < ApplicationController
   def home
   require "uri"
@@ -56,6 +13,6 @@ class HomeController < ApplicationController
 
   response = https.request(request)
   puts response.read_body
-  @data = JSON.parse(response)
+  @data = JSON.parse(response.read_body)
   end
 end
