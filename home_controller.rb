fd74c23dc37554e27bac5159adb5c1c6e93ab079
Aaron-Bruce <63589893+Aaron-Bruce@users.noreply.github.com>
2022-03-31 11:03:13 +0100

    Attempting to POST user location to home controller

---
+++ home_controller.rb
@@ -0,0 +1,21 @@
+require "uri"
+require "json"
+require "net/http"
+require_relative "../../API_KEY.rb"
+class HomeController < ApplicationController
+  def home
+  longitude = params(:longitude)
+  latitude = params(:latitude)
+  uri_string = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=53.3444407%2C-6.2595753&radius=750&type=restaurant&key=#{KEY}"
+  url = URI(uri_string)
+
+  https = Net::HTTP.new(url.host, url.port)
+  https.use_ssl = true
+
+  request = Net::HTTP::Get.new(url)
+
+  response = https.request(request)
+  data = JSON.parse(response.read_body)
+  @locations = data["results"]
+  end
+end
