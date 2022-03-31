36c34c33964264f3811da7bf31157659d05eae4a
Aaron-Bruce <63589893+Aaron-Bruce@users.noreply.github.com>
2022-03-31 11:21:49 +0100

    Delete routes.rb

--- routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "home#home"
-  get "/home", to: "home#home"
-  get "/places", to: "home#home"
-end
