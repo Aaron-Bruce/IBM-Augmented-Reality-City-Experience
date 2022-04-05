ff3b5ae5937f04065e3f30f9e05f65b0ef869999
Conn Breathnach <cobreath@tcd.ie>
2022-04-05 12:18:07 +0100

    Some things showing up in chrome

--- public/javascript/places.js
+++ public/javascript/places.js
@@ -1,41 +1,20 @@
-
-        // first get current user location
- navigator.geolocation.getCurrentPosition(function (position) {
+navigator.geolocation.getCurrentPosition(function (position) {
     //Reload page with new url  only if no location is set
-    if (window.location.href.indexOf("?") == -1) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-             $.ajax({
-         type:"GET",
-         url:"home/",
-         dataType:"json",
-         data: {"latitude": latitude, "longitude": longitude}
-     });
-        window.location.href = window.location.href + "?latitude=" + latitude + "&longitude=" + longitude;
-    }
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+    // if (window.location.href.indexOf("?") == -1) {
+    //     var latitude = position.coords.latitude;
+    //     var longitude = position.coords.longitude;
+    //     $.ajax({
+    //         type:"GET",
+    //         url:"home/",
+    //         dataType:"json",
+    //         data: {"latitude": latitude, "longitude": longitude}
+    //     });
+    //     window.location.href = window.location.href + "?latitude=" + latitude + "&longitude=" + longitude;
+    // }
 
-    
+
 
 
 });
-
-    // var latitude = position.coords.latitude;
-    // var longitude = position.coords.longitude;
-    //  $.ajax({
-    //      type:"GET",
-    //      url:"home/",
-    //      dataType:"json",
-    //      data: {"latitude": latitude, "longitude": longitude}
-    //  })
-    //             // window.open("/data_update?latitude="+latitude+"&longitude="+longitude,"self");
-
-                //$.ajax({
-                //    url: "/home/?latitude="+latitude+"&longitude"+longitude,
-                //    type: "POST",
-                //    data: values
-               //});
-// });
-
-
-
-
